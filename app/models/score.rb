8bdac5b192313cb9acb9a337bdce0077d48abc29
Bogdan Moise <bogdan.moise@golfgenius.com>
2022-08-24 21:02:55 +0300

    Add number of holes

--- app/models/score.rb
+++ app/models/score.rb
@@ -4,7 +4,9 @@
 class Score < ApplicationRecord
   belongs_to :user
 
-  validates :total_score, inclusion: { in: 54..120 }
+  validates :total_score, inclusion: { in: 27..180 }
+  validates :number_of_holes, inclusion: { in: [9, 18] }
+  validate :range_score
   validate :future_score
 
   def serialize
@@ -14,6 +16,7 @@ class Score < ApplicationRecord
       user_name: user.name,
       total_score: total_score,
       played_at: played_at,
+      number_of_holes: number_of_holes
     }
   end
 
@@ -21,5 +24,12 @@ class Score < ApplicationRecord
 
   def future_score
     errors.add(:played_at, 'must not be in the future') if played_at > Time.zone.today
+  end
+
+  def range_score
+    interval = 0
+    interval = (27..89) if number_of_holes == 9
+    interval = (54..179) if number_of_holes == 18
+    errors.add(:total_score, "must be between #{interval}") unless interval.cover? total_score
   end
 end
